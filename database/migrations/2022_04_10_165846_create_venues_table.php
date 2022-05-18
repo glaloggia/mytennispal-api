2817206d43a33b522d15b9cbdafd145f78101bc3
glaloggia-lyit <L00169720@student.lyit.ie>
2022-05-18 13:13:02 +0100

    Make the venue address unique in the database definition.

--- database/migrations/2022_04_10_165846_create_venues_table.php
+++ database/migrations/2022_04_10_165846_create_venues_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('venues', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('address');
+            $table->string('address')->unique();
             $table->timestamps();
         });
     }
